ffaa5aea4032d3803826c538fe4d2021719c24b1
salott15 <sarilott@yahoo.com>
2017-03-31 18:02:13 -0400

    new commit

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ var oAuth
 
 $('document').ready(function(){
 	console.log(window.location.href);
-	oAuth = window.location.href.replace(/^.*token=/g,'');
-	console.log(oAuth);
+	if(window.location.href.indexOf("access_token")>-1){
+    oAuth = window.location.href.replace(/^.*token=/g,'');
+    localStorage.setItem("instagram", oAuth);
+	 console.log(oAuth);
+  };
+  $(".instagram").hide();
+  $(".nyt").hide();
+  if($(localStorage.instagram).exists()){
+    $(".login").hide();
+  }
 })
 
-
+$("input").keyup(function(e){
+  if(e.keyCode === 13){
+    $("button").click();
+  }
+});
 
 $("button").click(function(){
+  $(".centered").removeClass();
+  $(".instagram").html("");
+  $(".nyt").html("");
 	loadNyt();
 	loadInstagram();
 });
@@ -24,77 +39,53 @@ function loadNyt(){
   		method: 'GET',
 	}).done(function(result) {
   	console.log(result);
+    callbackNyt(result);
 	}).fail(function(err) {
  	 throw err;
  	});
+  $(".nyt").show();
  }
 
  function loadInstagram(){
- 	var url = "https://api.instagram.com/v1/tags/" + $("input").val() + 
- 	"?access_token=" + oAuth
+ 	//var url = "https://api.instagram.com/v1/tags/" + $("input").val() + "/media/recent" +
+ 	//"?access_token=" + localStorage.instagram
+  var url = "https://api.instagram.com/v1/tags/" + $("input").val() + "/media/recent" +
+  "?access_token=" + localStorage.instagram
+  console.log(url);
  	$.ajax({
  		dataType: 'jsonp',
   		url: url,
   		method: 'GET',
 	}).done(function(result) {
   	console.log(result);
+    callbackInsta(result);
 	}).fail(function(err) {
  	 throw err;
  	});
+  $(".instagram").show();
  }
-	//Spotify, will pick up later. 
-	//$.ajax({
-	//	url: "https://api.spotify.com/v1/browse/categories/" + $('input').val(),
-	//	headers: {
-	//		Authorization: "AQDygyh6AYTxYRxnDhVw5GRNj6XneNc37bV9I85_GhVwOlXhlyxK3L2OMDZoBbTvDqEdjNevgTYVRUYohfV4dqgy4e6IQX7__eq5-XsTKsfuC5juVXD-0XShrzf3tzfHdyhhp49jn_nYU6SDBeldQ9NxEweszwOhILABMrU-EOYMGgzti2XaKbcimIwdh9e-GCY6T7_6sNJMziY0B8zN"
-	//	}
-	//})
-	//$.getJSON("https://api.spotify.com/v1/browse/categories/" + $('input').val(), obj, callback);
 
-//function loadSpotify(){
-//var request = request('require'); // "Request" library
-
-//var client_id = '41330404e28d4c8eb6bde974de89f30a'; 
-//var client_secret = 'cec1265923c44ca2865d2c0ce57f8382'; 
-
-// your application requests authorization
-//var authOptions = {
-  //url: 'https://accounts.spotify.com/api/token',
-  //headers: {
-    //'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  //},
-//  form: {
- //   grant_type: 'client_credentials'
-//  },
-//  json: true
-//};
-
-//request.post(authOptions, function(error, response, body) {
-//  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-//    var token = body.access_token;
-//    var options = {
-//      url: 'https://api.spotify.com/v1/users/jmperezperez',
-//      headers: {
-//        'Authorization': 'Bearer ' + token
-//      },
-//      json: true
-//    };
-//    request.get(options, function(error, response, body) {
-//      console.log(body);
-//    });
-//  }
-//})
-//}
-
-function callback(data){
+function callbackInsta(data){
 	console.log(data);
-	for(i=0; i<data.items.length; i++){
-		$(".instagram").append("<a href='" + "https://youtube.com/watch?v=" + data.items[i].id.videoId + "'>Click to watch video</a>" + "<br>")
-		$(".instagram").append(data.items[i].snippet.title + "<br>")
-		$(".instagram").append("<img src= '" + data.items[i].snippet.thumbnails.default.url + "'/>" + "<br>")
-	}
+  $(".instagram").append("<h1>Instagram</h1>" + "<p>Photos taken in " + $("input").val() +"</p>")
+	//for(i=0; i<data.data.length; i++){
+	//	$(".instagram").append('<img src=' + data.data[i].images.standard_resolution.url + '>' + "<br>")
+	//	$(".instagram").append(data.data[i].caption.text + "<br>")
+	//}
+  for(i=0; i<10; i++){
+    $(".instagram").append('<img src = "http://fpoimg.com/300x250">' + '<br>')
+    $(".instagram").append("FPOimg" + "<br>")
+  }
 }
 
+
+function callbackNyt(data){
+  console.log(data);
+  $(".nyt").append("<h1>The New York Times</h1>" + "<p>Recent articles about " + $("input").val() +"</p>")
+  for(i=0; i<data.response.docs.length; i++){
+    $(".nyt").append("<img src ='http://www.nytimes.com/" + data.response.docs[i].multimedia[1].url + "'><br>")
+    $(".nyt").append("<h2>" + data.response.docs[i].headline.main + "</h2>")
+    $(".nyt").append("<a href = '" + data.response.docs[i].web_url + "'>" + data.response.docs[i].snippet + "</a>" + "<br>")
+  }
+}
 
